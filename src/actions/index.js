b1439cefa78ef4c49c9bad35264d230fec7db1af
João Amaral <jamaral@pasonatquila.com>
2017-05-22 14:59:19 +0900

    Version with Line Charts

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
-const API_KEY = '???'; //change API_KEY
+//const API_KEY = '???'; //change API_KEY
+const API_KEY = '7f130abcbce4b9f3fab866737f760c99';
+
 const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
@@ -8,6 +10,9 @@ export const FETCH_WEATHER = 'FETCH_WEATHER';
 export function fetchWeather(city) {
   const url = `${ROOT_URL}&q=${city}`;
   const request = axios.get(url);
+
+  console.log('Request: ', request);
+
   return {
     type: FETCH_WEATHER,
     payload: request
